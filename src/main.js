7bc8d7e32d5129bcbefd673cedc19df68175518b
Darlington Leonel <darlingtonleonel@gmail.com>
2025-08-29 13:28:51 -0300

    Fazendo Back-End

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-"use strict"
+'use strict';
 
 //CSS
 import "./styles/style.css"
+
+//JS
+import './modules/form.js';
+import './modules/page-load.js';
---
+++ src/services/api-config.js
@@ -0,0 +1,3 @@
+export const apiConfig = {
+    baseURL: process.env.NODE_ENV === 'http://localhost:3000'
+}
